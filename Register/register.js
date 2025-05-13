dd25a157b3e04e45b5a369afcee833069622b32a
NikolaPavleski <ddpavleski@gmail.com>
2025-05-13 21:04:42 +0200

    Delete Register directory

--- Register/register.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("registerForm").addEventListener("submit", function (event) {
-    event.preventDefault();
-    alert("Your account has been successfully registered!");
-  });
