35298d1b01bb38fa3337dfd9dcf207fba5b40b2b
NikolaPavleski <ddpavleski@gmail.com>
2025-05-26 22:45:16 +0200

    Add files via upload

---
+++ Login/register.js
@@ -0,0 +1,4 @@
+document.getElementById("registerForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Your account has been successfully registered!");
+  });
