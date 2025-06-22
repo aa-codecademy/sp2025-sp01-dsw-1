896357ef68991df6f8d70e4b6b7958e657b552da
kristijansofronijoski <kristijansofronijoski@gmail.com>
2025-06-22 21:37:06 +0200

    Sorting and Filtering

--- src/scripts/category.js
+++ src/scripts/category.js
@@ -6,6 +6,15 @@ const paginationContainer = document.getElementById("pagination");
 const livingRoomBtn = document.getElementById("living-room-btn");
 const bedroomBtn = document.getElementById("bedroom-btn");
 const diningRoomBtn = document.getElementById("dining-room-btn");
+const stockBtnFilter = document.getElementById("stock-btn-filter");
+const livingRoomBtnFilter = document.getElementById("livingroom-btn-filter");
+const bedroomBtnFilter = document.getElementById("bedroom-btn-filter");
+const diningRoomBtnFilter = document.getElementById("diningroom-btn-filter");
+const azBtnSort = document.getElementById("a-z-sort");
+const zaBtnSort = document.getElementById("z-a-sort");
+const lowestFirstSort = document.getElementById("lowest-first-sort");
+const highestFirstSort = document.getElementById("highest-first-sort");
+
 
 const productsPerPage = 16;
 let allProducts = [];
@@ -94,6 +103,51 @@ function filterByCategory(category) {
   setActivePage(1);
 }
 
+function filterByStock(){
+  filteredProducts = allProducts.filter((p) => p.stock > 0);
+  renderProducts(1);
+  setupPagination(filteredProducts.length);
+  setActivePage(1);
+}
+
+function sortByAtoZ(){
+  filteredProducts = [...allProducts].sort((a,b) =>a.name.localeCompare(b.name));
+  renderProducts(1);
+  setupPagination(filteredProducts.length);
+  setActivePage(1);
+}
+
+function sortByZtoA(){
+   filteredProducts = [...allProducts].sort((a,b) =>b.name.localeCompare(a.name));
+  renderProducts(1);
+  setupPagination(filteredProducts.length);
+  setActivePage(1);
+}
+
+function sortByLowestFirst() {
+  filteredProducts = [...allProducts].sort((a, b) => {
+    const priceA = parseInt(a.price.replace(/[^\d]/g, ""));
+    const priceB = parseInt(b.price.replace(/[^\d]/g, ""));
+    return priceA - priceB;
+  });
+
+  renderProducts(1);
+  setupPagination(filteredProducts.length);
+  setActivePage(1);
+}
+function sortByHighestFirst() {
+  filteredProducts = [...allProducts].sort((a, b) => {
+    const priceA = parseInt(a.price.replace(/[^\d]/g, ""));
+    const priceB = parseInt(b.price.replace(/[^\d]/g, ""));
+    return priceB - priceA;
+  });
+  renderProducts(1);
+  setupPagination(filteredProducts.length);
+  setActivePage(1);
+}
+
+
+
 fetch("../assets/products.json")
   .then((response) => response.json())
   .then((data) => {
@@ -116,6 +170,42 @@ diningRoomBtn.addEventListener("click", (e) => {
   e.preventDefault();
   filterByCategory("dining-room");
 });
+livingRoomBtnFilter.addEventListener("click", (e) => {
+  e.preventDefault();
+  filterByCategory("living-room");
+});
+bedroomBtnFilter.addEventListener("click", (e) => {
+  e.preventDefault();
+  filterByCategory("bedroom");
+});
+diningRoomBtnFilter.addEventListener("click", (e) => {
+  e.preventDefault();
+  filterByCategory("dining-room");
+});
+stockBtnFilter.addEventListener("click", (e) => {
+  e.preventDefault();
+  filterByStock();
+});
+
+azBtnSort.addEventListener("click", (e) =>{
+  e.preventDefault();
+  sortByAtoZ();
+});
+
+zaBtnSort.addEventListener("click", (e) =>{
+  e.preventDefault();
+  sortByZtoA();
+});
+
+lowestFirstSort.addEventListener("click", (e) => {
+  e.preventDefault();
+  sortByLowestFirst();
+});
+
+highestFirstSort.addEventListener("click", (e =>{
+  e.preventDefault();
+  sortByHighestFirst();
+}));
 
 loadHTML("../templates/header.html", "afterbegin");
 loadHTML("../templates/footer.html", "beforeend");
